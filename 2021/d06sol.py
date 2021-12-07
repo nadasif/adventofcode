4e9adbb907d0c31791f78fe072c47603d89469a9
Asif Nadeem <asif_nad@hotmail.com>
2021-12-07 10:27:00 -0700

    For day06 changed the dict to indexed list

--- 2021/d06sol.py
+++ 2021/d06sol.py
@@ -3,27 +3,26 @@ import logging.config
 logging.config.fileConfig('../logging.conf')
 logger = logging.getLogger(__name__)
 
+
 class School:
    
    def __init__(self):
-      self.counts = {str(f'f({i})'): 0 for i in range(9)}
-      
+      self.counts = [0 for i in range(9)]
+   
    def add(self, txt):
-      self.counts[f'f({txt})'] += 1
-      
+      self.counts[int(txt)] += 1
+   
    def dayPassed(self):
-      f0 = self.counts['f(0)']
+      f0 = self.counts[0]
       for n in range(8):
-         self.counts[f'f({n})'] = self.counts[f'f({n+1})']
-      self.counts['f(6)'] += f0
-      self.counts['f(8)'] = f0
-      print(self.counts)
-      
+         self.counts[n] = self.counts[n + 1]
+      self.counts[6] += f0
+      self.counts[8] = f0
+      # print(self)
+   
    def count(self):
-      return sum(self.counts.values())
+      return sum(self.counts)
 
-   def __repr__(self):
-      return self.counts.__repr__()
 
 def fileLines(filename):
    file1 = open(filename, 'r')
@@ -33,19 +32,18 @@ def fileLines(filename):
 
 
 def main():
-
    school = School()
-
+   
    lines = fileLines('d06in.txt')
    for txt in lines[0].split(','):
       school.add(txt)
    
-   print(school)
+   logger.debug('%s', school)
    for d in range(256):
       school.dayPassed()
    
    print(school.count())
-   
+
 
 if __name__ == '__main__':
    main()
