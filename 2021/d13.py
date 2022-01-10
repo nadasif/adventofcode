a9ff80a188300f6c389791b3d876e19d275d2d03
Asif Nadeem <asif_nad@hotmail.com>
2022-01-09 21:34:32 -0700

    day 13 converted to sheet object

--- 2021/d13.py
+++ 2021/d13.py
@@ -2,103 +2,100 @@ import os
 
 
 class Sheet:
-   commands = []
-   map = [[]]
-   rows = 0
-   cols = 0
    
-   @staticmethod
-   def set(c, r):
-      Sheet.rows = max(Sheet.rows, r + 1)
-      Sheet.cols = max(Sheet.cols, c + 1)
-      while len(Sheet.map) <= r:
-         Sheet.map.append([0 for _ in range(Sheet.cols)])
-      row = Sheet.map[r]
+   def __init__(self):
+      self.commands = []
+      self.map = [[]]
+      self.rows = 0
+      self.cols = 0
+   
+   def set(self, c: int, r: int):
+      self.rows = max(self.rows, r + 1)
+      self.cols = max(self.cols, c + 1)
+      while len(self.map) <= r:
+         self.map.append([0 for _ in range(self.cols)])
+      row = self.map[r]
       while len(row) <= c:
          row.append(0)
-      Sheet.map[r][c] = 1
+      self.map[r][c] = 1
    
-   @staticmethod
-   def foldAll():
-      for cmd in Sheet.commands:
-         Sheet.fold(cmd)
+   def foldAll(self):
+      self.normalize()
+      for cmd in self.commands:
+         self.fold(cmd)
    
-   @staticmethod
-   def fold(cmd: str):
+   def fold(self, cmd: str):
       (c, n) = cmd.strip().split('=')
       n = int(n)
       if c == 'fold along y':
-         Sheet.foldAlongY(n)
+         self.foldAlongY(n)
       else:
-         Sheet.foldAlongX(n)
+         self.foldAlongX(n)
    
-   @staticmethod
-   def foldAlongX(n):
+   def foldAlongX(self, n):
       t = 0
-      for i in range(Sheet.rows):
-         row = Sheet.map[i]
+      for i in range(self.rows):
+         row = self.map[i]
          k = 1
-         while (n - k) >= 0 or (n + k) < Sheet.cols:
+         while (n - k) >= 0 or (n + k) < self.cols:
             row[n - k] |= row[n + k]
             t += row[n - k]
             k += 1
-      Sheet.cols = n
+      self.cols = n
       print(f'Moved along x {n} ({t} dots)')
-
-   @staticmethod
-   def foldAlongY(n):
+   
+   def foldAlongY(self, n):
       t = 0
       k = 1
-      while (n - k) >= 0 or (n + k) < Sheet.rows:
-         row1 = Sheet.map[n - k]
-         row2 = Sheet.map[n + k]
-         for i in range(Sheet.cols):
+      while (n - k) >= 0 or (n + k) < self.rows:
+         row1 = self.map[n - k]
+         row2 = self.map[n + k]
+         for i in range(self.cols):
             row1[i] |= row2[i]
             t += row1[i]
          k += 1
-      Sheet.rows = n
+      self.rows = n
       print(f'Moved along y {n} ({t} dots)')
-
-   @staticmethod
-   def showMap():
+   
+   def showMap(self):
       dotHash = [' ', chr(0x258b)]
       s = ''
-      for r in range(Sheet.rows):
-         row = Sheet.map[r]
+      for r in range(self.rows):
+         row = self.map[r]
          s += '\n'
-         for c in range(Sheet.cols):
-            if len(row) <= Sheet.cols:
+         for c in range(self.cols):
+            if len(row) <= self.cols:
                row.append(0)
             s += dotHash[row[c]]
       print(f'Map: {s}')
+   
+   def normalize(self):
+      for r in range(self.rows):
+         row = self.map[r]
+         for c in range(self.cols):
+            if len(row) <= self.cols:
+               row.append(0)
 
 
 def main():
-   loadData('.in')
-   Sheet.foldAll()
-   Sheet.showMap()
+   sheet = Sheet()
+   loadData('.in', sheet)
+   sheet.foldAll()
+   sheet.showMap()
 
 
-def loadData(ext):
+def loadData(ext: str, sheet: Sheet):
    filename = os.path.splitext(__file__)[0] + ext
    file = open(filename, 'r')
    for line in file.readlines():
       s = line.strip()
       if s.startswith('fold'):
-         Sheet.commands.append(s)
+         sheet.commands.append(s)
       elif s == '':
          pass
       else:
          arg = tuple(int(k) for k in s.split(','))
-         Sheet.set(*arg)
-
-   for r in range(Sheet.rows):
-      row = Sheet.map[r]
-      for c in range(Sheet.cols):
-         if len(row) <= Sheet.cols:
-            row.append(0)
-
-
+         sheet.set(*arg)
 
 if __name__ == "__main__":
    main()
