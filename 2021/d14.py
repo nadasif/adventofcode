af28ed4895b5cf809a5235dcb44ba07cedc23901
Asif Nadeem <asif_nad@hotmail.com>
2022-01-14 00:04:39 -0700

    day 14 part 2 is done

--- 2021/d14.py
+++ 2021/d14.py
@@ -3,62 +3,69 @@ from collections import defaultdict
 
 
 class Poly:
-   def __init__(self):
-      self.template = []
-      self.pairs = dict()
-      pass
-   
-   def read(self, s: str):
-      if s == '':
-         return
-      pair = s.split(' -> ')
-      if len(pair) > 1:
-         self.pairs[pair[0]] = pair[1]
-      else:
-         self.template = s
-      pass
-   
-   def step(self):
-      s = self.template[0]
-      count = defaultdict(int)
-      count[s[0]] = 1
-      for i in range(1, len(self.template)):
-         left = self.template[i - 1]
-         right = self.template[i]
-         pair = left + right
-         center = self.pairs[pair]
-         s = f'{s}{center}{right}'
-         count[center] += 1
-         count[right] += 1
-      self.template = s
-      temp = sorted(count.items(), key= lambda v: v[1])
-      lo = temp[0]
-      hi = temp[len(temp)-1]
-      print(f'{hi[1] - lo[1]} {s}')
-      #print(s)
+	def __init__(self):
+		self.template = []
+		self.pairs = dict()
+		pass
+	
+	def read(self, s: str):
+		if s == '':
+			return
+		pair = s.split(' -> ')
+		if len(pair) > 1:
+			self.pairs[pair[0]] = pair[1]
+		else:
+			self.template = s
+		pass
+	
+	def step(self, itr):
+		chain = self.template
+		pairs = self.pairs
+		count = defaultdict(int)
+		pCount1 = defaultdict(int)
+		count[chain[0]] = 1
+		for j in range(1, len(chain)):
+			pCount1[chain[j - 1] + chain[j]] += 1
+			count[chain[j]] += 1
+		
+		for i in range(itr):
+			keys = list(filter(lambda p: pCount1[p] > 0, pCount1.keys()))
+			pCount2 = defaultdict(int)
+			for k in keys:
+				l = k[0]
+				r = k[1]
+				c = pairs[k]
+				n = pCount1[k]
+				pCount2[l + c] += n
+				pCount2[c + r] += n
+				count[c] += n
+			pCount1 = pCount2
+		sums = sorted(count.items(), key=lambda v: v[1])
+		print(sums)
+		hi = sums[len(sums) - 1][1]
+		lo = sums[0][1]
+		print(f'{hi} - {lo} = {hi - lo}')
 
 
 def main():
-   poly = Poly()
-   loadData('.in', poly)
-   print(poly.template)
-   for _ in range(10):
-      poly.step()
-   print('--------------- Part 1')
-   # for _ in range(30):
-   #    poly.step()
-
+	poly = Poly()
+	loadData('.in', poly)
+	print(poly.template)
+	poly.step(10)
+	print('--------------- Part 1')
+	poly.step(40)
+	print('--------------- Part 2')
 
 
 def loadData(ext: str, poly: Poly):
-   filename = os.path.splitext(__file__)[0] + ext
-   file = open(filename, 'r')
-   lines = file.readlines()
-   file.close()
-   
-   for line in lines:
-      poly.read(line.strip())
+	filename = os.path.splitext(__file__)[0] + ext
+	file = open(filename, 'r')
+	lines = file.readlines()
+	file.close()
+	
+	for line in lines:
+		poly.read(line.strip())
 
 
 if __name__ == "__main__":
-   main()
+	main()
