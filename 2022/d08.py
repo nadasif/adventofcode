7013595fc41f4e11990957dc75e5ecce2f140a77
Asif Nadeem <asif_nad@hotmail.com>
2022-12-22 21:59:57 -0700

    day 8 done

--- 2022/d08.py
+++ 2022/d08.py
@@ -1,18 +1,62 @@
 import os
 
-class Part1:
-    def __init__(self, txt):
-        pass
+
+class Cell:
+    def __init__(self, c):
+        self.n = int(c)
+        self.v = False
+        self.s = 0
 
     def __repr__(self):
-        return f"#<P1:>"
+        return f"{self.n}{'*' if self.v else ' '} "
+
 
 class Part2:
     def __init__(self, txt):
-        pass
+        self.grid = [[Cell(c) for c in s] for s in txt.split()]
+        self.rows = len(self.grid)
+        self.cols = len(self.grid[0])
+
+    def visibleCount(self):
+        s = 0
+        for r in range(self.rows):
+            for c in range(self.cols):
+                s += 1 if self.grid[r][c].v else 0
+
+        return s
+
+    def markVisible(self):
+        for r in range(self.rows):
+            self.grid[r][0].v = True
+            self.grid[r][self.cols-1].v = True
+            # horizontal
+            left = self.grid[r][0].n
+            right = self.grid[r][self.cols-1].n
+            for c in range(self.rows):
+                if self.grid[r][c].n > left:
+                    self.grid[r][c].v = True
+                    left = self.grid[r][c].n
+                if self.grid[r][self.cols-1-c].n > right:
+                    self.grid[r][self.cols - 1 - c].v = True
+                    right = self.grid[r][self.cols - 1 - c].n
+
+        for c in range(self.cols):
+            self.grid[0][c].v = True
+            self.grid[self.rows-1][c].v = True
+            # vertical
+            top = self.grid[0][c].n
+            bot = self.grid[self.rows-1][c].n
+            for r in range(self.rows):
+                if self.grid[r][c].n > top:
+                    self.grid[r][c].v = True
+                    top = self.grid[r][c].n
+                if self.grid[self.rows-1-r][c].n > bot:
+                    self.grid[self.rows - 1 - r][c].v = True
+                    bot = self.grid[self.rows - 1 - r][c].n
+
 
     def __repr__(self):
-        return f"#<P2:>"
+        return '\n'.join( ''.join(f"{cell}" for cell in cells) for cells in self.grid )
 
 
 def loadData(ext: str):
@@ -22,14 +66,17 @@ def loadData(ext: str):
     file.close()
     return data
 
+
 def main():
     txt = loadData('.in')
-    lines = txt.split('\n')
 
     print("\nPart 1")
+    grid = Part2(txt)
+    grid.markVisible()
+    print(grid)
+    print(grid.visibleCount())
 
     print("\nPart 2")
-
 
     pass
 
