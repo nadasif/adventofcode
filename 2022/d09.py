fbfc82a76e2aab9fb7e6a7048b6125561f362641
Asif Nadeem <asif_nad@hotmail.com>
2022-12-27 11:20:22 -0700

    day 9 working
    Done using OOP

--- 2022/d09.py
+++ 2022/d09.py
@@ -1,17 +1,55 @@
 import os
 
-hx, hy, tx, ty = 0,0,0,0
+class Command:
+    _dxdy = {'L': (-1,0), 'R': (1,0), 'U': (0,1), 'D': (0,-1)}
+    def __init__(self, txt):
+        self.op, step = txt.split()
+        self.dx, self.dy = Command._dxdy[self.op]
+        self.steps = int(step)
 
-def touching(x1, y1, x2, y2):
-    return abs(x2-x1) <= 1 and abs(y2-y1) <= 1
+    def __repr__(self):
+        return f"#<Command: {self.op} {self.steps}>"
 
-def move(dx, dy):
-    global hx, hy, tx, ty
-    hx += dx
-    hy += dy
-    if not touching(hx, hy, tx, ty):
-        tx += 0 if hx==tx else (hx-tx)/abs(hx-tx)
-        ty += 0 if hy==ty else (hy-ty)/abs(hy-ty)
+class Coord:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def touching(self, other):
+        return abs(self.x - other.x)<=1 and abs(self.y - other.y)<=1
+
+    def move(self, cmd: Command):
+        return Coord(self.x + cmd.dx, self.y + cmd.dy)
+
+    def follow(self, coord):
+        if self.touching(coord): return self
+        hx, hy = coord.x, coord.y
+        tx, ty = self.x, self.y
+        dx = 0 if hx == tx else (hx - tx) / abs(hx - tx)
+        dy = 0 if hy == ty else (hy - ty) / abs(hy - ty)
+        return Coord(tx + dx, ty + dy)
+
+    def __repr__(self):
+        return f"({self.x},{self.y})"
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y==other.y
+
+class Plane:
+    def __init__(self, n):
+        self.knots = [Coord(0,0) for _ in range(n)]
+        self.visited = set()
+        self.visited.add(self.knots[n-1])
+
+    def move(self, cmd: Command):
+        n = len(self.knots)
+        for _ in range(cmd.steps):
+            self.knots[0] = self.knots[0].move(cmd)
+            for i in range(1, n):
+                self.knots[i] = self.knots[i].follow(self.knots[i-1])
+            self.visited.add(self.knots[n-1])
 
 def loadData(ext: str):
     filename = os.path.splitext(__file__)[0] + ext
@@ -21,8 +59,6 @@ def loadData(ext: str):
     return data
 
 def main():
-    dxy = {'L': (-1,0), 'R':(1,0), 'U': (0,1), 'D': (0,-1)}
-
     txt = '''R 4
 U 4
 L 3
@@ -32,20 +68,27 @@ D 1
 L 5
 R 2'''
     txt = loadData('.in')
-    visited = set()
-    visited.add((tx,ty))
+    cmds = list(map(Command, txt.split('\n')))
+    # print(cmds)
 
-    lines = txt.split('\n')
-    for line in lines:
-        op, steps = line.split(' ')
-        steps = int(steps)
-        dx, dy = dxy[op]
+    print("\nPart 1")
+    plane = Plane(2)
 
-        for _ in range(steps):
-            move(dx, dy)
-            visited.add((tx,ty))
+    for cmd in cmds:
+        plane.move(cmd)
 
-    print(len(visited))
+    print(len(plane.visited))
+
+
+    print("\nPart 2")
+    plane = Plane(10)
+
+    for cmd in cmds:
+        plane.move(cmd)
+
+    print(len(plane.visited))
+
+
     pass
 
 
