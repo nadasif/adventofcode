5c27a7c092e39bb3dd5e8719e782c72618706b52
Asif Nadeem <asif_nad@hotmail.com>
2022-12-27 18:25:59 -0700

    day 10 done

--- 2022/d10.py
+++ 2022/d10.py
@@ -1,18 +1,15 @@
 import os
 
-class Part1:
+class Command:
     def __init__(self, txt):
-        pass
+        self.cycles = 1 if txt == 'noop' else 2
+        self.num = 0 if self.cycles == 1 else int(txt[5:])
+
+    def add(self, n):
+        return n + self.num
 
     def __repr__(self):
         return f"#<P1:>"
-
-class Part2:
-    def __init__(self, txt):
-        pass
-
-    def __repr__(self):
-        return f"#<P2:>"
 
 
 def loadData(ext: str):
@@ -24,12 +21,41 @@ def loadData(ext: str):
 
 def main():
     txt = loadData('.in')
-    lines = txt.split('\n')
+    cmds = list(map(Command, txt.split('\n')))
 
     print("\nPart 1")
+    cycle = 0
+    x = 1
+    s = 0
+    for cmd in cmds:
+        for c in range(cmd.cycles):
+            cycle += 1
+            if ((cycle + 20) % 40) == 0:
+                signal = x * cycle
+                s += signal
+                print(signal)
+        x = cmd.add(x)
+
+    print(s)
+
 
     print("\nPart 2")
+    crt = [' '] * 240
+    cycle = 0
+    x = 1
+    for cmd in cmds:
+        for c in range(cmd.cycles):
+            if x <= (cycle % 40 + 1) <= (x+2):
+                crt[cycle] = '#'
+            cycle += 1
+        x = cmd.add(x)
 
+    print(''.join(crt[0:40]))
+    print(''.join(crt[40:80]))
+    print(''.join(crt[80:120]))
+    print(''.join(crt[120:160]))
+    print(''.join(crt[160:200]))
+    print(''.join(crt[200:240]))
 
     pass
 
