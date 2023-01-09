876f2d981978354844cca559820b708936f58463
Asif Nadeem <asif_nad@hotmail.com>
2023-01-09 14:01:46 -0700

    2022 day 13 and 14

--- 2022/d13.py
+++ 2022/d13.py
@@ -1,18 +1,29 @@
 import os
+from functools import cmp_to_key
 
-class Part1:
-    def __init__(self, txt):
-        pass
 
-    def __repr__(self):
-        return f"#<P1:>"
+def comp(lt, rt):
+    # print(f' - Compare {lt} vs {rt}')
 
-class Part2:
-    def __init__(self, txt):
-        pass
+    if isinstance(lt, int) and isinstance(rt, int):
+        return 0 if lt == rt else -1 if lt < rt else 1
+    elif isinstance(lt, list) and isinstance(rt, int):
+        return comp(lt, [rt])
+    elif isinstance(lt, int) and isinstance(rt, list):
+        return comp([lt], rt)
 
-    def __repr__(self):
-        return f"#<P2:>"
+    i = 0
+    while i < len(lt) and i < len(rt):
+        c = comp(lt[i], rt[i])
+        if c != 0:
+            return c
+        i += 1
+    if i == len(lt) and i < len(rt):
+        return -1
+    elif i == len(rt) and i < len(lt):
+        return 1
+    else:
+        return 0
 
 
 def loadData(ext: str):
@@ -22,16 +33,58 @@ def loadData(ext: str):
     file.close()
     return data
 
+
 def main():
+    txt = '''[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]'''
     txt = loadData('.in')
-    lines = txt.split('\n')
+    lines = txt.split('\n\n')
+    s = 0
+    for n in range(len(lines)):
+        a, b = map(eval, lines[n].split("\n"))
+        # print()
+        if comp(a, b) == -1:
+            # print("Good")
+            s += (n + 1)
 
-    print("\nPart 1")
+    print("Part 1")
+    print(s)
 
-    print("\nPart 2")
+    lines = list(map(eval, txt.split()))
+    lines.append([[2]])
+    lines.append([[6]])
+    lines = sorted(lines, key=cmp_to_key(comp))
 
+    for i, lst in enumerate(lines):
+        #print(f"{i}: {lst}")
+        if lst == [[2]]:
+            a = i + 1
+        elif lst == [[6]]:
+            b = i + 1
 
-    pass
+    print("Part 2")
+    print(a * b)
 
 
 if __name__ == "__main__":
--- 2022/d14.py
+++ 2022/d14.py
@@ -1,11 +1,13 @@
 import os
 
+
 class Part1:
     def __init__(self, txt):
         pass
 
     def __repr__(self):
         return f"#<P1:>"
+
 
 class Part2:
     def __init__(self, txt):
@@ -22,16 +24,115 @@ def loadData(ext: str):
     file.close()
     return data
 
+
+def minmax(lst, x, y):
+    print(f"{lst}; {x}, {y} --> ", end='')
+    lst[0] = min([lst[0], x])
+    lst[1] = max([lst[1], x])
+    lst[2] = max([lst[2], y])
+    print(f"{lst};")
+
+def dot(x, y, rocks, drops):
+    p = (x, y)
+    if p in {(500, 0)}:
+        return '+'
+    elif p in rocks:
+        return '#'
+    elif p in drops:
+        return 'o'
+    else:
+        return '.'
+
+def drop2(filled, mxy):
+    x, y = 500, 0
+    if (x, y) in filled:
+        return False
+
+    while y <= mxy:
+        if (x, y + 1) not in filled:
+            y += 1
+            continue
+
+        if (x-1, y + 1) not in filled:
+            x -= 1
+            y += 1
+            continue
+
+        if (x+1, y + 1) not in filled:
+            x += 1
+            y += 1
+            continue
+
+        break
+
+    filled.add((x,y))
+    return True
+
+
+
+def drop(filled, mxy):
+    x, y = 500, 0
+    while y <= mxy:
+        ny = y + 1
+        if (x, ny) not in filled:
+            y += 1
+            continue
+
+        if (x-1, ny) not in filled:
+            x -= 1
+            y += 1
+            continue
+
+        if (x+1, ny) not in filled:
+            x += 1
+            y += 1
+            continue
+
+        filled.add((x,y))
+        return True
+    return False
+
 def main():
+    rocks = set()
+    txt = '''498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9'''
     txt = loadData('.in')
     lines = txt.split('\n')
+    mmc = [500, 500,0]
+    for line in lines:
+        coords = line.split(" -> ")
+        x1, y1 = eval(coords[0])
+        for i in range(1, len(coords)):
+            x2, y2 = eval(coords[i])
+            minmax(mmc, x2, y2)
+            if y1 != y2:
+                assert x1 == x2
+                ds = sorted([y1, y2])
+                for d in range(ds[0], ds[1] + 1):
+                    rocks.add((x1, d))
+            if x1 != x2:
+                assert y1 == y2
+                ds = sorted([x1, x2])
+                for d in range(ds[0], ds[1] + 1):
+                    rocks.add((d, y1))
 
+            x1, y1 = x2, y2
+
+    drops = set(rocks)
     print("\nPart 1")
+    i = 0
+    while drop(drops, mmc[2]):
+        i += 1
+    print(i)
+
+
+
+    drops = set(rocks)
+    while drop2(drops, mmc[2]):
+        pass
 
     print("\nPart 2")
-
-
-    pass
+    print(len(drops) - len(rocks))
 
 
 if __name__ == "__main__":
