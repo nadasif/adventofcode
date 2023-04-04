75e076de22551a2e3932e184d2805a5fb5880017
Asif Nadeem <asif_nad@hotmail.com>
2023-04-04 07:16:24 -0600

    201508 in progress

--- 2015/d08.py
+++ 2015/d08.py
@@ -1,12 +1,29 @@
 from lib.mylib import loadData
 
 
+def len0(line: str) -> int:
+    return len(eval(line))
+
+
+def len1(line: str) -> int:
+    return len(line)
+
+
+def len2(line: str) -> int:
+    return len(line) + line.count('\\') + line.count('"') + 2
+
+
 def main():
-    txt = loadData()
+    lines = loadData().split("\n")
+
+    chr_count = sum(map(len1, lines))
+    mem_count = sum(map(len0, lines))
 
     print("\nPart 1")
+    print(chr_count - mem_count)
 
     print("\nPart 2")
+    print(sum(map(len2, lines)) - chr_count)
 
 
 if __name__ == "__main__":
--- 2015/d09.py
+++ 2015/d09.py
@@ -1,9 +1,44 @@
+import re
+
 from lib.mylib import loadData
+
+
+class City:
+    cities = dict()
+
+    def __init__(self, name):
+        self.name = name
+        self.distances = dict()
+        City.cities[name] = self
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return hash(other) == hash(self)
+
+    def __repr__(self):
+        dists = ", ".join([f"{k}:{v}" for k, v in self.distances.items()])
+        return f'City({self.name!r} {dists})'
+
+    def set_distance(self, node, dist):
+        if self.distances.get(node.name) is None:
+            self.distances[node.name] = dist
+            node.set_distance(self, dist)
+
+    @classmethod
+    def create_city(cls, txt):
+        name1, name2, dist = re.split(r' to | = ', txt)
+        city1 = City.cities.get(name1) or cls(name1)
+        city2 = City.cities.get(name2) or cls(name2)
+        city1.set_distance(city2, int(dist))
 
 
 def main():
     txt = loadData()
-
+    list(map(City.create_city, txt.split('\n')))
+    for city in City.cities.values():
+        print(city)
     print("\nPart 1")
 
     print("\nPart 2")
