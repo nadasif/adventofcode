44ea6f0ae3974752f7ec689892789c657975cb39
Asif Nadeem <asif_nad@hotmail.com>
2023-04-09 11:33:46 -0600

    202001 done

--- 2020/d01.py
+++ 2020/d01.py
@@ -1,16 +1,32 @@
-import os
+from lib.mylib import loadData
 
 
-def loadData():
-    with open(f"in-{os.path.splitext(os.path.basename(__file__))[0]}.txt", "r") as f:
-        return f.read().strip()
+def find_2sum(lst: list[int], target: int) -> int:
+    seen = dict()
+    for n in lst:
+        if n in seen:
+            return seen[n] * n
+        seen[target - n] = n
+    return 0
+
+
+def find_3sum(unsorted_lst: list[int], target: int) -> int:
+    lst = sorted(unsorted_lst)
+    for i, n in enumerate(lst):
+        two_sum = find_2sum(lst[i + 1:], target - n)
+        if two_sum:
+            return two_sum * n
+    return 0
+
 
 def main():
-    txt = loadData()
-
+    nums = list(map(int, loadData().split('\n')))
+    # print(nums)
     print("\nPart 1")
+    print(find_2sum(nums, 2020))
 
     print("\nPart 2")
+    print(find_3sum(nums, 2020))
 
 
 if __name__ == "__main__":
