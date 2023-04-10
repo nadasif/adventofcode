6b582b78ad846c0c9675ad59c6bbd9d173b61977
Asif Nadeem <asif_nad@hotmail.com>
2023-04-10 15:15:40 -0600

    202005 done

--- 2020/d05.py
+++ 2020/d05.py
@@ -1,16 +1,42 @@
-import os
+from lib.mylib import loadData
 
 
-def loadData():
-    with open(f"in-{os.path.splitext(os.path.basename(__file__))[0]}.txt", "r") as f:
-        return f.read().strip()
+def decode(code):
+    rows, cols = list(range(128)), list(range(8))
+
+    for c in code:
+        if c == 'F':
+            rows = rows[:len(rows) // 2]
+        elif c == 'B':
+            rows = rows[len(rows) // 2:]
+        elif c == 'R':
+            cols = cols[len(cols) // 2:]
+        elif c == 'L':
+            cols = cols[:len(cols) // 2]
+    return rows[0] * 8 + cols[0]
+
+
+class Seat:
+    def __init__(self, code):
+        self.__code = code
+
+    def get_id(self):
+        return decode(self.__code)
+
 
 def main():
-    txt = loadData()
+    seats = list(map(Seat, loadData().split('\n')))
+
+    seat_ids = list(map(Seat.get_id, seats))
 
     print("\nPart 1")
+    max_id = max(seat_ids)
+    print(max_id)
 
     print("\nPart 2")
+    min_id = min(seat_ids)
+    my_seat, = set(range(min_id, max_id + 1)) - set(sorted(seat_ids))
+    print(my_seat)
 
 
 if __name__ == "__main__":
