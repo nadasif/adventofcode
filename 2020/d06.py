9f300ac35172531fc87f9d99e152fd9b6675f501
Asif Nadeem <asif_nad@hotmail.com>
2023-04-11 11:05:21 -0600

    202006 refactored
    Added Stream for mapping the map.

--- 2020/d06.py
+++ 2020/d06.py
@@ -1,16 +1,21 @@
 from functools import reduce
 
-from lib.mylib import loadData
+from lib.mylib import loadData, Stream
+
+
+def square(x):
+    return x ** 2
 
 
 def main():
     groups = loadData().split('\n\n')
-    print(groups)
     print("\nPart 1")
     print(sum(map(lambda s: len(set(s.replace('\n', ''))), groups)))
 
     print("\nPart 2")
-    print(sum(map(len, map(lambda group: reduce(lambda s1, s2: set(s1) & set(s2), group.split('\n')), groups))))
+    print(Stream(groups)
+          .map(lambda grp: reduce(lambda s1, s2: set(s1) & set(s2), grp.split('\n')))
+          .map(len).sum())
 
 
 if __name__ == "__main__":
--- lib/mylib.py
+++ lib/mylib.py
@@ -2,6 +2,20 @@ import inspect
 import re
 
 
+class Stream:
+    def __init__(self, iterable):
+        self.__iterable = iterable
+
+    def map(self, function):
+        return Stream(map(function, self.__iterable))
+
+    def list(self):
+        return list(self.__iterable)
+
+    def sum(self):
+        return sum(self.__iterable)
+
+
 def loadData() -> str:
     infile = re.sub(r"(d[0-9]{2})\.py", r"in-\g<1>.txt", inspect.stack()[1].filename)
     with open(infile, "r") as file:
