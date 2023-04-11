ce9e9dadb5452c95a82925c96e3820372b0de961
Asif Nadeem <asif_nad@hotmail.com>
2023-04-11 07:47:19 -0600

    202006 done

--- 2020/d06.py
+++ 2020/d06.py
@@ -1,16 +1,27 @@
-import os
+from functools import reduce
 
+from lib.mylib import loadData
 
-def loadData():
-    with open(f"in-{os.path.splitext(os.path.basename(__file__))[0]}.txt", "r") as f:
-        return f.read().strip()
 
 def main():
-    txt = loadData()
-
+    groups = loadData().split('\n\n')
+    print(groups)
     print("\nPart 1")
+    print(sum(map(lambda s: len(set(s.replace('\n', ''))), groups)))
 
     print("\nPart 2")
+    print()
+
+    print(len(groups))
+    total = 0
+    for members in groups:
+        all_yes = set(members)
+        for member in members.split('\n'):
+            all_yes &= set(member)
+        #print(len(all_yes))
+        total += len(all_yes)
+    print(total)
+
 
 
 if __name__ == "__main__":
